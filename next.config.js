08d2013d047e9b2798742a9b4f1b167fc31380c5
James Richmond <jblackenmusic@gmail.com>
2024-01-09 09:05:34 -0700

    updated images remote pattern

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
       // Will be available on both server and client
         assetPrefix: '',
     },
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'basic-portfolio-0yaa.onrender.com', // Change this to your domain or S3 bucket
+            port: '',
+            pathname: '/public/images/**', // Adjust the path to your images
+          },
+        ],
+    }
 }
     
 module.exports = nextConfig
