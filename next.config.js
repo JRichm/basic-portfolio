2bc8b1e1eeacd72af075bd96ffca8ac6b1061251
James Richmond <jblackenmusic@gmail.com>
2024-01-09 13:55:52 -0700

    setup next.config for static export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    // output: 'export',
-    // publicRuntimeConfig: {
-    //   // Will be available on both server and client
-    //     assetPrefix: '',
-    // },
+    output: 'export',
+    publicRuntimeConfig: {
+      // Will be available on both server and client
+        assetPrefix: '',
+    },
 }
     
 module.exports = nextConfig
