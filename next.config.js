ca5b64f8f3c4169d4305bb842f4e4ff9e494e10a
James Richmond <jblackenmusic@gmail.com>
2024-01-09 08:46:42 -0700

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     output: 'export',
     publicRuntimeConfig: {
       // Will be available on both server and client
-      STATIC_PATH: '/public',
+        assetPrefix: '',
     },
 }
     
